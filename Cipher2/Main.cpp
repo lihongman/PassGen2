ee53dc100f1ec075e1211064adcc4cd5778d2ec5
lihongman <herman_hmli@yahoo.com>
2017-06-26 23:12:01 -1000

    Added increased randomness
    Added exclamation and added sum of characters into rotation count.

--- Cipher2/Main.cpp
+++ Cipher2/Main.cpp
@@ -2,20 +2,28 @@
 #include <string>
 
 char getChar(int chara) {
-	while (!(47 < chara && chara < 58) && !(59 < chara && chara < 91) && !(96 < chara && chara < 123)) {
+	while (chara != 33 && !(47 < chara && chara < 58) && !(59 < chara && chara < 91) && !(96 < chara && chara < 123)) {
 		if (chara > 122) {
 			chara = chara % 122;
 		}
-		chara += 10;
+		chara += 21;
 	}
 	return chara;
+}
+
+int sum(std::string& inputs) {
+	int sums = 0;
+	for (unsigned int i = 0; i < inputs.length(); ++i) {
+		sums += inputs[i];
+	}
+	return sums;
 }
 
 void Rotate(std::string& password) {
 	int front = 0, back = password.length()-1;
 	while ((back - front) > 1) {
-		password[front] = getChar(password[front] + password[back]*2);
-		password[back] = getChar(password[back] + password[front]*2);
+		password[front] = getChar(password[front] + sum(password) + password[back]*2);
+		password[back] = getChar(password[back] + +sum(password) + password[front]*2);
 		++front;
 		--back;
 	}
